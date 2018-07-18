a1203e157152585f577c3fd15d8928a5a67cba1c
lizhangrui <ja131484@gmail.com>
2018-07-18 23:22:48 +0800

    升级至webpack4 添加postcss, 动态添加dll

--- bin/li-tools.js
+++ bin/li-tools.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const { spawn }  = require('child_process')
+const path = require('path')
 const packageJson = require('../package.json')
 
 function range(val) {
@@ -20,24 +22,19 @@ function increaseVerbosity(v, total) {
   return total + 1;
 }
 
-program
-  .version('0.0.1')
-  .usage('[options] <file ...>')
-  .option('-i, --integer <n>', 'An integer argument', parseInt)
-  .option('-f, --float <n>', 'A float argument', parseFloat)
-  .option('-r, --range <a>..<b>', 'A range', range)
-  .option('-l, --list <items>', 'A list', list)
-  .option('-o, --optional [value]', 'An optional value')
-  .option('-c, --collect [value]', 'A repeatable value', collect, [])
-  .option('-v, --verbose', 'A value that can be increased', increaseVerbosity, 0)
-  .parse(process.argv);
+// program
+//   .version('0.0.1')
+//   .usage('[options] <file ...>')
+//   .command('start', 'start a dev server', )
+//   .option('-i, --integer <n>', 'An integer argument', parseInt)
+//   .option('-f, --float <n>', 'A float argument', parseFloat)
+//   .option('-r, --range <a>..<b>', 'A range', range)
+//   .option('-l, --list <items>', 'A list', list)
+//   .option('-o, --optional [value]', 'An optional value')
+//   .option('-c, create [value]', 'A repeatable value', collect, [])
+//   .option('-v, --verbose', 'A value that can be increased', increaseVerbosity, 0)
+//   .parse(process.argv)
 
-console.log(' int: %j', program.integer);
-console.log(' float: %j', program.float);
-console.log(' optional: %j', program.optional);
-program.range = program.range || [];
-console.log(' range: %j..%j', program.range[0], program.range[1]);
-console.log(' list: %j', program.list);
-console.log(' collect: %j', program.collect);
-console.log(' verbosity: %j', program.verbose);
-console.log(' args: %j', program.args);
+// program.range = program.range || []
+
+spawn('node', [path.resolve(__dirname, '../lib/server.js')], { stdio: 'inherit' })
