ef92c3dedfa20b065f73bfa26980e6e9228875ee
lizhangrui <ja131484@gmail.com>
2018-07-23 03:01:09 +0800

    添加 examples

---
+++ examples/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructors(props) {
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return 'react App'
+  }
+}
+
+export default App
---
+++ examples/webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  resolve: {
+    alias: {
+      'li-tools': '../packages/li-tools/bin/li-tools',
+    }
+  }
+}
