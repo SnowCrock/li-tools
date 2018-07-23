18ede84e58da9f7fedeac83822168820f07a9367
lizhangrui <ja131484@gmail.com>
2018-07-23 10:55:32 +0800

    更改webpack打包时的一些提示

--- packages/li-tools/lib/build.js
+++ packages/li-tools/lib/build.js
@@ -3,6 +3,26 @@ const webpackConfig = require('./config/webpack.config.prod')
 
 const compiler = webpack(webpackConfig)
 
-compiler.run(() => {
-  
+compiler.run((err, stats) => {
+  if(err) {
+    console.log(err)
+    process.exit(1)
+  }
+
+  // https://webpack.docschina.org/configuration/stats/#stats 
+    // 具体参数配置可在这找
+    console.log(stats.toString({
+      chunks: false,  // 使构建过程更静默无输出
+      colors: true,    // 在控制台展示颜色
+      cached: false,
+      cachedAssets: false,
+      chunkOrigins: false,
+      chunkModules: false,
+      modules: false,
+      entrypoints: false,
+      children: false, // 添加 children 信息
+      source: false,
+      excludeModules: /\.map/,
+      timings: true,
+    }))
 })
